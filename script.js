7227240636803c12a8e459cd4d7eb1b9c6bcf6f5
rishimangang <rishimangang809@gmail.com>
2026-08-19 23:26:50 +0530

    Add smooth scrolling for anchor links
    battleground manipur official game website

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(function(link) {
+
+  link.addEventListener('click', function(event) {
+
+    const target = document.querySelector(
+      link.getAttribute('href')
+    );
+
+    if (target) {
+      event.preventDefault();
+
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+
+  });
+
+});
